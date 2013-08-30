afec87a676ddb4973d49665d89eb8af1d453cf9d
Nicola Iarocci <nicola@nicolaiarocci.com>
2013-08-30 17:44:44 +0200

    Huge refactoring [WIP]

--- src/RESTDataReader.cs
+++ src/RESTDataReader.cs
@@ -48,14 +48,7 @@ namespace DataAccess.RESTDataAccess
 			_client.UserAgent = "eve-csharp/" + version; 
             _client.AddDefaultHeader("Accept-charset", "utf-8");
 	        _client.AddHandler("application/json", new JsonDeserializer());
-
 			_client.BaseUrl = DataSourceName;
-			if (Authentication != null) 
-			{
-				_client.Authenticator = new HttpBasicAuthenticator (Authentication.UserName, 
-				                                                    Authentication.Password);
-			}
-
 		}
 
 		/// <summary>
@@ -65,10 +58,7 @@ namespace DataAccess.RESTDataAccess
 		/// <typeparam name="T">The type to be returned.</typeparam>
 		public override Response<T> Get<T> (GetRequest request)
 		{ 
-			RestRequest r = new RestRequest ();
-			r.Resource = request.Resource;
-
-			return Execute<T> (r);
+			return Execute<T> (ProcessRequestBase(request));
 		}
 
 		/// <summary>
@@ -78,7 +68,30 @@ namespace DataAccess.RESTDataAccess
 		/// <typeparam name="T">The type to be returned.</typeparam>
 		public override Response<T> Get<T>(GetRequestItem request) 
 		{ 
-			throw new NotImplementedException ();
+//			return Execute<T> (ProcessRequestBase(request));
+//			throw new NotImplementedException ();
+			var restRequest = ProcessRequestBase(request);
+			restRequest.Resource = string.Format ("/{0}/{1}/", restRequest.Resource, request.Id);
+			restRequest.DateFormat = "ddd, dd MMM yyyy HH:mm:ss UTC";
+
+			return Execute<T> (restRequest);
+		}
+
+		private RestRequest ProcessRequestBase(RequestBase request)
+		{
+			var restRequest = new RestRequest ();
+
+			if (request.Resource != null)
+				restRequest.Resource = request.Resource;
+			else
+				throw new ArgumentNullException ("Resource");
+
+			if (request.Authentication != null) 
+				_client.Authenticator = new HttpBasicAuthenticator (request.Authentication.UserName, request.Authentication.Password);
+			else if (Authentication != null) 
+				_client.Authenticator = new HttpBasicAuthenticator (Authentication.UserName, Authentication.Password);
+
+			return restRequest;
 		}
 
 		private Response<T> Execute<T>(RestRequest request) where T: new()
@@ -88,29 +101,41 @@ namespace DataAccess.RESTDataAccess
 				{
 					// for individual resources when there's an error to make
 					// sure that RestException props are populated
-					if (((int)resp.StatusCode) >= 400)
-					{
-						// have to read the bytes so .Content doesn't get populated
-	                    string restException = "{{ \"RestException\" : {0} }}";
-						var content = resp.RawBytes.AsString(); //get the response content
-	                    var newJson = string.Format(restException, content);
-
-	                    resp.Content = null;
-						resp.RawBytes = Encoding.UTF8.GetBytes(newJson.ToString());
-					}
+//					if (((int)resp.StatusCode) >= 400)
+//					{
+//						// have to read the bytes so .Content doesn't get populated
+//	                    string restException = "{{ \"RestException\" : {0} }}";
+//						var content = resp.RawBytes.AsString(); //get the response content
+//	                    var newJson = string.Format(restException, content);
+//
+//	                    resp.Content = null;
+//						resp.RawBytes = Encoding.UTF8.GetBytes(newJson.ToString());
+//					}
 				};
 
-		  	RestResponse<T> restResponse = (RestResponse<T>)_client.Execute<T> (request);
-			Response<T> response = new Response<T>();
+			if (DataSourceName != null)
+				_client.BaseUrl = DataSourceName;
+			else
+				throw new ArgumentNullException ("DataSourceName");
+
+			return ProcessResponse ((RestResponse<T>)_client.Execute<T> (request));
+		}
+
+		private Response<T> ProcessResponse<T>(RestResponse<T> restResponse)
+		{
+			var response = new Response<T>();
 			response.ErrorException = restResponse.ErrorException;
 			response.ErrorMessage = restResponse.ErrorMessage;
 			response.StatusDescription = restResponse.StatusDescription;
-			response.ResponseStatus = (DataAccess.ResponseStatus)restResponse.ResponseStatus;
 			response.Content = restResponse.Data;
+
+			// TODO this explicit cast is tricky as we're talking to different classes here, although they are
+			// identical.
+			response.ResponseStatus = (DataAccess.ResponseStatus)restResponse.ResponseStatus;
 
 			// TODO StatusCode enum needs better documentation and completion (how to handle the default case?)
 			switch (restResponse.StatusCode) {
-			case HttpStatusCode.Accepted:
+			case HttpStatusCode.OK:
 				response.StatusCode = StatusCode.Accepted;
 				break;
 			case HttpStatusCode.Ambiguous:
@@ -120,9 +145,8 @@ namespace DataAccess.RESTDataAccess
 				response.StatusCode = StatusCode.NotAvailable;
 				break;
 			}
-
 			return response;
 		}
-	}
+	} 
 }
 
